1c92ae63a5a6007455238f34e8ae2640ab1ae860
Denis Gonçalves Marques <denwork@outlook.com>
2018-10-10 18:44:22 -0300

    Adjust PEP8 requirements for marks_movie_trailers.py

--- media.py
+++ media.py
@@ -1,13 +1,18 @@
 import webbrowser
 
+
 class Movie:
     """This class provides movie trailer information"""
-    
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url): 
+
+    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
-        self.trailer_youtube_url=  trailer_youtube_url
+        self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)  
+        webbrowser.open(self.trailer_youtube_url)
+
+    def set_trailer_youtube_url(self,url):
+        self.trailer_youtube_url = url
+
--- movie_trailers_manager.py
+++ movie_trailers_manager.py
@@ -1,43 +1,58 @@
 import media
 import marks_movie_trailers
 
-# List of movies
-matrix_reloaded = media.Movie(
-  "Matrix reloaded",
-  "A story a hacker to save the world",
-  "https://m.media-amazon.com/images/M/MV5BMGZjNGQ4MmItM2MwZS00YWE1LThhNDQtN2RhY2QwNTk4YmVmXkEyXkFqcGdeQXVyODA1NjQ0OTY@._V1_.jpg",
-  "https://www.youtube.com/watch?v=kYzz0FSgpSU")
 
-gladiator = media.Movie(
-  "Gladiator",    
-  "A Roman General fight your honor as gladiator",
-  "https://upload.wikimedia.org/wikipedia/en/thumb/8/8d/Gladiator_ver1.jpg/220px-Gladiator_ver1.jpg",
-  "https://www.youtube.com/watch?v=uvbavW31adA")
+def create_movie_trailers():
+    matrix_reloaded = media.Movie("Matrix reloaded",
+                                  "A story a hacker to save the world",
+                                  "https://m.media-amazon.com/images/M" +
+                                  "/MV5BMGZjNGQ4MmItM2MwZS00YWE1LThhND" +
+                                  "QtN2RhY2QwNTk4YmVmXkEyXkFqcGdeQXVyO" +
+                                  "DA1NjQ0OTY@._V1_.jpg",
+                                  "https://www.youtube.com/watch?v=kYzz" +
+                                  "0FSgpSU")
 
-indiana_jones = media.Movie(
-  "Indiana Jones",
-  "Treasure hunters and pulp action heroes",
-  "https://images-na.ssl-images-amazon.com/images/I/61F7bV5N9xL.jpg",
-  "https://www.youtube.com/watch?v=HqOSLZl9GUo")  
+    gladiator = media.Movie("Gladiator",
+                            "A Roman General fight your honor as gladiator",
+                            "https://upload.wikimedia.org/wikipedia/en/" +
+                            "thumb/8/8d/Gladiator_ver1.jpg/220px-Gladia" +
+                            "tor_ver1.jpg",
+                            "https://www.youtube.com/watch?v=uvbavW31adA")
 
-pursuit_happyness = media.Movie(  
-  "The Pursuit of Happyness",
-  "Gardner becomes obstinate for the survival and sustenanc e of his family",
-  "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1e/The_Pursuit_of_Happyness.jpg/200px-The_Pursuit_of_Happyness.jpg",
-  "https://www.youtube.com/watch?v=89Kq8SDyvfg")
+    indiana_jones = media.Movie("Indiana Jones",
+                                "Treasure hunters and pulp action heroes",
+                                "https://images-na.ssl-images-amazon.com/" +
+                                "images/I/61F7bV5N9xL.jpg",
+                                "https://www.youtube.com/watch?v=HqOSLZl9GUo")
 
-who_am_i = media.Movie(
-  "Jackie Chan's Who Am I?",
-  "Guy lost memory, but have mission to complet",
-  "https://resizing.flixster.com/xs7Ac2TA-eaZalo45XkmDBaZZHc=/206x305/v1.bTsxMjE4NDgzNTtqOzE3ODI2OzEyMDA7Mjg0OzQwNQ",
-  "https://www.youtube.com/watch?v=Gdx55aI-BLw")
+    pursuit_happyness = media.Movie("The Pursuit of Happyness",
+                                    "Gardner becomes obstinate for the " +
+                                    "survival and sustenanc e of his family",
+                                    "https://upload.wikimedia.org/wikipedia" +
+                                    "/pt/thumb/1/1e/The_Pursuit_of_Happynes" +
+                                    "s.jpg/200px-The_Pursuit_of_Happyness.jpg",
+                                    "https://www.youtube.com/watch" +
+                                    "?v=89Kq8SDyvfg")
 
-terminator2 = media.Movie(
-  "Terminator 2",
-  "The protection came the future",
-  "https://http2.mlstatic.com/poster-cartaz-exterminador-do-futuro-2-terminator-40x60cm-D_NQ_NP_686685-MLB25905408285_082017-O.jpg",
-  "https://www.youtube.com/watch?v=lwSysg9o7wE")  
+    who_am_i = media.Movie("Jackie Chan's Who Am I?",
+                           "Guy lost memory, but have mission to complet",
+                           "https://resizing.flixster.com/xs7Ac2TA-eaZalo" +
+                           "45XkmDBaZZHc=/206x305/v1.bTsxMjE4NDgzNTtqOzE3" +
+                           "ODI2OzEyMDA7Mjg0OzQwNQ",
+                           "https://www.youtube.com/watch?v=Gdx55aI-BLw")
 
-movies = [matrix_reloaded, gladiator, indiana_jones, pursuit_happyness, who_am_i, terminator2]  
+    terminator2 = media.Movie("Terminator 2",
+                              "The protection came the future",
+                              "https://http2.mlstatic.com/poster-cartaz-ext" +
+                              "erminador-do-futuro-2-terminator-40x60cm-D_NQ" +
+                              "_NP_686685-MLB25905408285_082017-O.jpg",
+                              "https://www.youtube.com/watch?v=lwSysg9o7wE")
+
+    return [matrix_reloaded, gladiator, indiana_jones,
+            pursuit_happyness, who_am_i, terminator2]
+
+
+# Populate site with data
+movies = create_movie_trailers()
 
 marks_movie_trailers.open_movies_page(movies)
